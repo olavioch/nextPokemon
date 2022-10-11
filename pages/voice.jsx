2cfd168ff4785d4d9f1889167243ca78c1b45de9
olavioch <olavo.resende@raccoon.ag>
2022-10-11 01:35:24 -0300

    voice not button

--- pages/voice.jsx
+++ pages/voice.jsx
@@ -4,7 +4,7 @@ export default function Voice(){
     return <>
         <p id="output"></p>
         <p id='test'></p>
-        <button id="start">pesquise no google por voz</button>
+        <p id="start">pesquise no google por voz</p>
         <Script
             id='voice'
             strategy="afterInteractive"
@@ -13,25 +13,22 @@ export default function Voice(){
                     const test = document.querySelector('#test');
                     const startBtn = document.querySelector('#start');
                     const output = document.querySelector('#output');
-                    function start() {
-                        const recognition = new webkitSpeechRecognition();
-                        recognition.interimResults = true;
-                        recognition.lang = "pt-BR";
-                        recognition.continuous = true;
-                        recognition.start();
-                        // This event happens when you talk in the microphone
-                        recognition.onresult = function(event) {
-                            for (let i = event.resultIndex; i < event.results.length; i++) {
-                                if (event.results[i].isFinal) {
-                                    // Here you can get the string of what you told
-                                    const content = event.results[i][0].transcript.trim();
-                                    output.textContent = 'voce falou: ' + content;
-                                    window.open('https://www.google.com/search?q='+content, '_blank')
-                                }
+                    const recognition = new webkitSpeechRecognition();
+                    recognition.interimResults = true;
+                    recognition.lang = "pt-BR";
+                    recognition.continuous = true;
+                    recognition.start();
+                    // This event happens when you talk in the microphone
+                    recognition.onresult = function(event) {
+                        for (let i = event.resultIndex; i < event.results.length; i++) {
+                            if (event.results[i].isFinal) {
+                                // Here you can get the string of what you told
+                                const content = event.results[i][0].transcript.trim();
+                                output.textContent = 'voce falou: ' + content;
+                                window.open('https://www.google.com/search?q='+content, '_blank')
                             }
-                        };
+                        }
                     };
-                    startBtn.addEventListener('click', () => start());
             `}}
         />
     </>
