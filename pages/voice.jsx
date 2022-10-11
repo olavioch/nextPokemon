08565aab73d2bc2ceb5fd7356f29755383c18804
olavioch <olavo.resende@raccoon.ag>
2022-10-11 01:22:23 -0300

    finished script voice

---
+++ pages/voice.jsx
@@ -0,0 +1,38 @@
+import Script from "next/script"
+
+export default function Voice(){
+    return <>
+        <p id="output"></p>
+        <p id='test'></p>
+        <button id="start">pesquise no google por voz</button>
+        <Script
+            id='voice'
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+            __html: `
+                    const test = document.querySelector('#test');
+                    const startBtn = document.querySelector('#start');
+                    const output = document.querySelector('#output');
+                    function start() {
+                        const recognition = new webkitSpeechRecognition();
+                        recognition.interimResults = true;
+                        recognition.lang = "pt-BR";
+                        recognition.continuous = true;
+                        recognition.start();
+                        // This event happens when you talk in the microphone
+                        recognition.onresult = function(event) {
+                            for (let i = event.resultIndex; i < event.results.length; i++) {
+                                if (event.results[i].isFinal) {
+                                    // Here you can get the string of what you told
+                                    const content = event.results[i][0].transcript.trim();
+                                    output.textContent = 'voce falou: ' + content;
+                                    window.open('https://www.google.com/search?q='+content, '_blank')
+                                }
+                            }
+                        };
+                    };
+                    startBtn.addEventListener('click', () => start());
+            `}}
+        />
+    </>
+}
